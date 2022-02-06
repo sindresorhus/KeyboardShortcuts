482c1795cfb39b4d9dcae5c0c9344d7130280b94
Adrian Schoenig <adrian@schoenig.me>
2022-02-06 21:19:24 +1100

    Fix indentation

--- Sources/KeyboardShortcuts/Shortcut.swift
+++ Sources/KeyboardShortcuts/Shortcut.swift
@@ -298,55 +298,55 @@ extension KeyboardShortcuts.Shortcut: CustomStringConvertible {
 // MARK: - SwiftUI Helpers
 
 extension KeyboardShortcuts.Shortcut {
-  
-  @available(iOS 14.0, macOS 11.0, *)
-  var swiftUI: SwiftUI.KeyboardShortcut {
-    .init(.init(keyEquivalent.first!), modifiers: modifiers.swiftUI)
-  }
-
+	
+	@available(iOS 14.0, macOS 11.0, *)
+	var swiftUI: SwiftUI.KeyboardShortcut {
+		.init(.init(keyEquivalent.first!), modifiers: modifiers.swiftUI)
+	}
+	
 }
 
 @available(macOS 10.15, *)
 extension NSEvent.ModifierFlags {
-  var swiftUI: SwiftUI.EventModifiers {
-    var modifiers: SwiftUI.EventModifiers = []
-    if contains(.shift) {
-      modifiers.insert(.shift)
-    }
-    if contains(.command) {
-      modifiers.insert(.command)
-    }
-    if contains(.capsLock) {
-      modifiers.insert(.capsLock)
-    }
-    if contains(.function) {
-      modifiers.insert(.function)
-    }
-    if contains(.option) {
-      modifiers.insert(.option)
-    }
-    if contains(.control) {
-      modifiers.insert(.control)
-    }
-    return modifiers
-  }
+	var swiftUI: SwiftUI.EventModifiers {
+		var modifiers: SwiftUI.EventModifiers = []
+		if contains(.shift) {
+			modifiers.insert(.shift)
+		}
+		if contains(.command) {
+			modifiers.insert(.command)
+		}
+		if contains(.capsLock) {
+			modifiers.insert(.capsLock)
+		}
+		if contains(.function) {
+			modifiers.insert(.function)
+		}
+		if contains(.option) {
+			modifiers.insert(.option)
+		}
+		if contains(.control) {
+			modifiers.insert(.control)
+		}
+		return modifiers
+	}
 }
 
 @available(iOS 14.0, macOS 11.0, *)
 extension View {
-  
-  @ViewBuilder
-  /// Assigns the global keyboard shortcut to the modified control.
-  ///
-  /// Only assigns a keyboard shortcut, if one was defined (or it has a default shortcut).
-  ///
-  /// - Parameter shortcut: Strongly-typed name of the shortcut
-  public func keyboardShortcut(_ shortcut: KeyboardShortcuts.Name) -> some View {
-    if let shortcut = (shortcut.shortcut ?? shortcut.defaultShortcut)?.swiftUI {
-      self.keyboardShortcut(shortcut)
-    } else {
-      self
-    }
-  }
-  
+	
+	@ViewBuilder
+	/// Assigns the global keyboard shortcut to the modified control.
+	///
+	/// Only assigns a keyboard shortcut, if one was defined (or it has a default shortcut).
+	///
+	/// - Parameter shortcut: Strongly-typed name of the shortcut
+	public func keyboardShortcut(_ shortcut: KeyboardShortcuts.Name) -> some View {
+		if let shortcut = (shortcut.shortcut ?? shortcut.defaultShortcut)?.swiftUI {
+			self.keyboardShortcut(shortcut)
+		} else {
+			self
+		}
+	}
+	
 }
