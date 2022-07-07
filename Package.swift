0994810987dba8783274f2b5c2a407a4d4dea8c8
Sindre Sorhus <sindresorhus@gmail.com>
2022-07-07 20:29:22 +0200

    Use Swift Package Index for docs

--- Package.swift
+++ Package.swift
@@ -15,9 +15,6 @@ let package = Package(
 			]
 		)
 	],
-	dependencies: [
-		.package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
-	],
 	targets: [
 		.target(
 			name: "KeyboardShortcuts"
