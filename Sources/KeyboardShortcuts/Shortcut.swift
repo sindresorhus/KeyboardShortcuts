2b5ac4e57df7cc9ab36660cbd1b89406893b6721
Matthias Gansrigler <opensource@eternalstorms.at>
2022-08-01 15:15:54 +0200

    makes Shortcut.istakenBySystem public

--- Sources/KeyboardShortcuts/Shortcut.swift
+++ Sources/KeyboardShortcuts/Shortcut.swift
@@ -104,7 +104,7 @@ extension KeyboardShortcuts.Shortcut {
 	/**
 	Check whether the keyboard shortcut is already taken by the system.
 	*/
-	var isTakenBySystem: Bool {
+	public var isTakenBySystem: Bool {
 		guard self != Self(.f12, modifiers: []) else {
 			return false
 		}
