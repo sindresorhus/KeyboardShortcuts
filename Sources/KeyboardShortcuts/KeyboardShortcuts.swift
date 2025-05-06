558ec6efbf27a2676ed6639cf7910b467b564626
Robert Hahn <877263+Maschina@users.noreply.github.com>
2025-05-06 14:32:21 +0200

    Update documentation in KeyboardShortcuts.swift

--- Sources/KeyboardShortcuts/KeyboardShortcuts.swift
+++ Sources/KeyboardShortcuts/KeyboardShortcuts.swift
@@ -183,10 +183,12 @@ public enum KeyboardShortcuts {
 
 	/**
 	Remove the keyboard shortcut handler for the given name.
-	
+
 	This can be used to reset the handler before re-creating it to avoid having multiple handlers for the same shortcut.
-	
+
 	- Parameter name: The name of the keyboard shortcut to remove handlers for.
+
+	- Note: This method does not affect listeners using `.on()`.
 	*/
 	public static func removeHandler(for name: Name) {
 		legacyKeyDownHandlers[name] = nil
